f1f3fd490f6c0e77f7d7e65110707944825f1e5c
Jayesh <jpworksdemo@gmail.com>
2021-12-27 15:40:44 +0530

    Commit

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -2,21 +2,37 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/type/Currency",
-    "../model/formatter"
-], function (Controller, JsonModel, Currency, formatter) {
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, Currency, formatter, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
         formatter: formatter,
-        
+
         /**
          * @override
          */
         onInit: function () {
-            var oViewModel = new JsonModel({
+            var oViewModel = new JSONModel({
                 Currency: "EUR"
             });
             this.getView().setModel(oViewModel, "view");
-        }
+        },
+        onFilterInvoices : function (oEvent) {
+
+            // build filter array
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
+
+            // filter binding
+            var oList = this.byId("invoiceList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+		}
     });
 });
